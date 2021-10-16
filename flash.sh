380841a6cfd402f720f549e965aecea54d935937
Juha Ylikoski <juha.ylikoski@tuni.fi>
2021-10-16 21:26:16 +0300

    Changed flash.sh to create .bin file to target/

--- flash.sh
+++ flash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-objcopy -O binary -I elf32-little target/riscv32imac-unknown-none-elf/$1/first-flash-longan-nano firmware.bin
-dfu-util -a 0 -s 0x08000000:leave -D firmware.bin
+objcopy -O binary -I elf32-little target/riscv32imac-unknown-none-elf/$1/first-flash-longan-nano target/riscv32imac-unknown-none-elf/$1/firmware.bin
+dfu-util -a 0 -s 0x08000000:leave -D target/riscv32imac-unknown-none-elf/$1/firmware.bin
